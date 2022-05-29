7a9d2fa3c4629380242be7710da21ff753255f47
dayoungMM <dalbeetm@gmail.com>
2022-05-29 15:17:11 +0900

    init

---
+++ src/main/java/com/moondysmell/yanoljaclone/YanoljaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.moondysmell.yanoljaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanoljaCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanoljaCloneApplication.class, args);
+	}
+
+}
