5f321ebf8dcf2e5a0cecdcf060f1db12cda42804
smellhyang <dlgiddk10@gmail.com>
2022-06-03 21:41:37 +0900

    fetch_join 버그 fixed - 둘 이상의 컬렌션은 fetch join사용 어려움 - DTO select로 직접 조회

--- src/main/java/com/moondysmell/yanoljaclone/repository/reservation/ReservationRepository.java
+++ src/main/java/com/moondysmell/yanoljaclone/repository/reservation/ReservationRepository.java
@@ -16,14 +16,14 @@ public interface ReservationRepository extends JpaRepository<Reservation, Any>{
     //@Query(value = "select distinct r from Reservation r join fetch r.accom ")
     //@Query(value = "select com.moondysmell.yanoljaclone.domain.Reservation(r.reserv_id, r.checkout) " +
     //jpaquery, querydsl, tuple, native query
-     @Query(value = "select a.accom_name, r.reserv_id, r.checkin, r.checkout , r.trans_type, r.room_cnt, a.price, u.name, u.phone_num, r.payment_type, a.detail  " +
-                   // " from Reservation r " +
-                   // "join Accommodation a on r.accom.id = a.id " +
-                   // "join Customer u on u.user_id = r.customer.user_id " +
-                   // "where u.name = :name and r.reserv_id = :reserv_id and u.phone_num = :phone_num ")
-                    "from Reservation r " +
-                    "join fetch r.accom a" +
-                    "join fetch r.customer c " +
-                     "where c.name = :name and r.reserv_id = :reserv_id and c.phone_num = :phone_num ")
+     @Query(value = "select new com.moondysmell.yanoljaclone.domain.dto.ReservationResponseDto( r.reserv_id, a.accom_name, r.checkin, r.checkout , r.trans_type, r.room_cnt, a.price, u.name, u.phone_num, r.payment_type, a.detail ) " +
+                    " from Reservation r " +
+                    "join Accommodation a on r.accom.id = a.id " +
+                    "join Customer u on u.user_id = r.customer.user_id " +
+                    "where u.name = :name and r.reserv_id = :reserv_id and u.phone_num = :phone_num ")
+                    //"from Reservation r " +
+                    //"join r.accom a" +
+                    //"join r.customer c " +
+                     //"where c.name = :name and r.reserv_id = :reserv_id and c.phone_num = :phone_num ")
     List<ReservationResponseDto> findReservedDetail(@Param("name")String name, @Param("reserv_id")int reserv_id, @Param("phone_num")String phone_num);
 }
