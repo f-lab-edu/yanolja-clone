627ecc807f515ba55968b21a5362becc992d22dd
dayoungMM <dalbeetm@gmail.com>
2022-06-05 21:01:02 +0900

    feat: accommodation 테스트 코드 추가 - getAll - createAccom - createRoom

--- src/test/java/com/moondysmell/yanoljaclone/accommodation/AccomTest.java
+++ src/test/java/com/moondysmell/yanoljaclone/accommodation/AccomTest.java
@@ -1,34 +1,69 @@
 package com.moondysmell.yanoljaclone.accommodation;
 
-import com.moondysmell.yanoljaclone.ServerApplicationTests;
+
+import static java.util.stream.Collectors.toSet;
+
 import com.moondysmell.yanoljaclone.domain.Accommodation;
 import com.moondysmell.yanoljaclone.domain.RoomType;
+import com.moondysmell.yanoljaclone.domain.dto.AccomAddDto;
+import com.moondysmell.yanoljaclone.domain.dto.RoomAddDto;
 import com.moondysmell.yanoljaclone.repository.AccommodationRepository;
+import com.moondysmell.yanoljaclone.service.AccomService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-public class AccomTest extends ServerApplicationTests {
+@SpringBootTest
+@Slf4j
+public class AccomTest {
 
     @Autowired
-    private AccommodationRepository repository;
-/*
+    private AccomService accomService;
+
     @Test
-    public void create(){
-        Accommodation accom = new Accommodation();
-        accom.setAccom_name("강화도바다향기");
-        accom.setAccom_code("A123");
-        accom.setLocation_code(01);
-        accom.setAddress("경기도 인천광역시 강화도");
-        accom.setRoom_type(RoomType.hotel);
-        accom.setRoom_name("바다방");
-        accom.setRoom_cnt(3);
-        accom.setPrice(70000);
-        accom.setDetail("깨끗하고 넓은방");
+    public void getAll() {
+        List<Accommodation> accomList = accomService.findAllByLocationCode(10);
 
-        Accommodation newAccom = repository.save(accom);
-        Assertions.assertNotNull(newAccom);
-
-
+        Map<String, Set<Accommodation>> groupedAccomList = accomList.stream()
+                                                               .collect(
+                                                                   Collectors.groupingBy(
+                                                                       Accommodation::getAccomCode,
+                                                                       toSet()));
+        log.info("groupedAccomList keySet: " + groupedAccomList.keySet().toString());
     }
-*/
+
+    @Test
+    public void addAccom() {
+        AccomAddDto newAccom = AccomAddDto.builder()
+                                              .accomCode(accomService.createAccomCode())
+                                              .accomName("산속의산")
+                                              .locationCode(20)
+                                              .address("인천 계양구")
+                                              .type(RoomType.Pension.toString())
+                                              .roomName("스탠다드")
+                                              .roomCnt(10)
+                                              .price(50000)
+                                              .build();
+        Accommodation createdAccom = accomService.createAccom(newAccom);
+        log.info("created Accom >>> " + createdAccom);
+    }
+
+    @Test
+    public void addRoom() {
+        RoomAddDto newRoom = RoomAddDto.builder()
+                                            .accomCode("37c678ac")
+                                            .roomName("대형방")
+                                            .roomCnt(1)
+                                            .price(100000)
+                                            .build();
+
+        Accommodation createdRoom = accomService.createRoom(newRoom);
+        log.info(createdRoom.toString());
+    }
 }
