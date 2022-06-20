2c0e4c98bfae33ca2a44cd7df38318bbea642dc0
smellhyang <dlgiddk10@gmail.com>
2022-06-20 22:06:38 +0900

    msa-gateway : yanolja service

--- src/main/java/com/moondysmell/yanoljaclone/YanoljaCloneApplication.java
+++ src/main/java/com/moondysmell/yanoljaclone/YanoljaCloneApplication.java
@@ -2,11 +2,18 @@ package com.moondysmell.yanoljaclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class YanoljaCloneApplication {
+	
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(YanoljaCloneApplication.class, args);
